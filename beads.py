a352746e0ce41761f05d3453bc7e2059eec31b55
max <thewolffpack21@gmail.com>
2019-09-05 22:44:53 -0700

    way harder :(

--- beads.py
+++ beads.py
@@ -1,16 +1,24 @@
 import argparse
+import math
+
+def regular_idx(idx, total):
+	if math.copysign(1, idx) < 0:
+		idx = total - abs(idx)
+	return idx
 
 # necklace & split index --> count from one side
-def count_one_way(necklace, ind, reverse):
+def count_one_way(necklace, ind, reverse, idxs_covered):
 	num_beads = len(necklace)
 	first = necklace[ind]
-	count = 1
+	count = 0
 	j = ind
 	k = 0
 	if reverse:
 		it_step = -1
 	else:
 		it_step = 1
+
+	idxs_covered.append(regular_idx(ind, num_beads))
 
 	while k < num_beads:
 
@@ -28,16 +36,15 @@ def count_one_way(necklace, ind, reverse):
 		if not special_case:
 			next_idx = j + it_step
 
-		if (necklace[next_idx] == first) or (necklace[next_idx] == 'w'):
+		if ((necklace[next_idx] == first) or (necklace[next_idx] == 'w')) and (regular_idx(next_idx, num_beads) not in idxs_covered):
 			count+=1
-
 		else:
 			break
 
 		j += it_step
 		k += 1
-		
-	return count
+	print('done')
+	return count, idxs_covered
 
 def main(necklace):
 
@@ -45,11 +52,16 @@ def main(necklace):
 	best_count = 0
 	best_split = 0
 
+
 	for i in range(num_beads-1):
-		current_count = 0
-		forward_count = count_one_way(necklace=necklace, ind=i+1, reverse=False)
-		backward_count = count_one_way(necklace=necklace, ind=i, reverse=True)
+		idxs = []
+		current_count = 2
+		forward_count, idxs = count_one_way(necklace=necklace, ind=i+1, reverse=False, idxs_covered=idxs)
+		print(idxs)
+		backward_count, _ = count_one_way(necklace=necklace, ind=i, reverse=True, idxs_covered=idxs)
 		current_count = forward_count + backward_count
+		print(forward_count)
+		print(backward_count)
 		if current_count > best_count:
 			best_count = current_count
 			best_split = i
